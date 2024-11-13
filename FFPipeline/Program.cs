099e41fad0c0195a3746519d0fe4b3b5a8fdd45b
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2024-11-13 15:25:25 -0600

    new project

---
+++ FFPipeline/Program.cs
@@ -0,0 +1,10 @@
+﻿using ConsoleAppFramework;
+
+var app = ConsoleApp.Create();
+app.Add("capabilities", async () =>
+{
+    await Task.Delay(100);
+    Console.WriteLine("Capabilities...");
+});
+
+app.Run(args);
